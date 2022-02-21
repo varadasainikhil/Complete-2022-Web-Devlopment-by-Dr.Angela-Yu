648cc742ba545404ebf659fccb178782dc055772
Sai Nikhil Varada <varadasainikhil@gmail.com>
2022-02-21 23:02:54 +0530

    Create isLeap.js

---
+++ isLeap.js
@@ -0,0 +1,8 @@
+function isLeap(year) {
+    if((year%4 == 0 && year%100 != 0 )|| (year%4 == 0 && year % 400 == 0 )){
+        return "Leap year.";
+    }
+    else{
+        return "Not leap year.";
+    }
+}
