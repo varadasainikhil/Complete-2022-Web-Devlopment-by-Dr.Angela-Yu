7c6191699f72f79f58fd94f220c69d35861e052c
Sai Nikhil Varada <varadasainikhil@gmail.com>
2022-02-21 23:07:26 +0530

    Delete isLeap.js

--- isLeap.js
+++
@@ -1,8 +0,0 @@
-function isLeap(year) {
-    if((year%4 == 0 && year%100 != 0 )|| (year%4 == 0 && year % 400 == 0 )){
-        return "Leap year.";
-    }
-    else{
-        return "Not leap year.";
-    }
-}
