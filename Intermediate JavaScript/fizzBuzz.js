5752b2e809bba67cb1044cc0748e3367a51654c2
Sai Nikhil Varada <varadasainikhil@gmail.com>
2022-02-22 07:19:46 +0530

    Create fizzBuzz.js

---
+++ Intermediate JavaScript/fizzBuzz.js
@@ -0,0 +1,15 @@
+for (let i = 1; i < 100; i++) {
+    if(i%3 ==0 && i%5!=0){
+        console.log("fizz"+i);
+    }
+    else if(i%5==0 && i%3==0){
+            console.log("fizzbuzz"+i);
+        }
+    else if(i%5==0 && i%3 !=0){
+        console.log("buzz"+i);
+    }
+    else{
+        continue;
+    }
+    
+}
